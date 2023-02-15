b1d40721eb636392999968df731f6e08509ff8df
AP696 <addyphilips66@gmail.com>
2023-02-15 11:48:15 +0000

    complete

--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,16 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(23)
+        expect(bag.weight).toBe(23)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    });
+
+    test('throws error when weight exceeds max', () => {
+
+        expect(() => new Bag(30)).toThrow('too heavy')
     })
 })
